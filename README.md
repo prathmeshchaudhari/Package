1025b845d3c6ea33c9ed6a46034d4bd035591057
Prathmesh Chaudhari <prathmesh299@gmail.com>
2017-07-21 23:20:25 +0530

    reinitialized files

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Package
+
+The goal of Package is to work with data from the fars database
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
 library(testthat)
+library(Package)
+test_check('Package')
 test_that("Return",{
   data(dt)
   expect_that(make_filename(2015), matches('accident_2015.csv.bz2'))
